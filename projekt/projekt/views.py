831dfc29990b4fed1d385e02eb231c65046eaf53
kolibrik7 <kolibrik7@gmail.com>
2017-05-02 20:36:52 +0200

    Add files via upload

---
+++ projekt/projekt/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+def index(request):
+	return render(request, "projekt/index.html")
+
+def registracia(request):
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get("username")
+			raw_password = form.cleaned_data.get("password1")
+			user = authenticate(username=username, password=raw_password)
+			login(request, user)
+			return redirect("index")
+	else:
+		form = UserCreationForm()
+
+	return render(request, "registration/registracia.html", {'form': form})
